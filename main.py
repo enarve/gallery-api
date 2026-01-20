11335d61c83365ad03da285604345f9c8e40d6c9
Nikita Vlaskin <sinezeleuny@gmail.com>
2026-01-20 20:04:01 +0100

    Dummy data

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+images_data = [
+    {
+        "name": "1.jpg",
+        "size": ""
+    },
+    {
+        "name": "2.jpg",
+        "size": ""
+    }
+]
+
 @app.get("/")
 def home():
     return {"message": "Hello, gallery!"}
+
+@app.get("/images")
+def images():
+    return images_data
